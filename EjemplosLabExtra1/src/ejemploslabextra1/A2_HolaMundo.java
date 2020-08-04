e1a2ac392d27f32e6055fab3edfcf97c079efc3b
ingridpm <69186270+ingridpm@users.noreply.github.com>
2020-08-04 00:52:21 -0600

    Eliminar

--- EjemplosLabExtra1/src/ejemploslabextra1/A2_HolaMundo.java
+++
@@ -1,8 +0,0 @@
-package ejemploslabextra1;
-
-public class A2_HolaMundo {
-    public static void main(String[] args) {
-        System.out.print("¡Hola mundo!");
-        System.out.println(":)");
-    }
-}
